8c07677eeceb29877ca3014824c62486f23bfe6d
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2020-12-09 12:51:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,7 @@ $subject = $HTTP_POST_VARS['subject'];
 $message = $HTTP_POST_VARS['message'];
 
 /* PHP form validation: the script checks that the Email field contains a valid email address and the Subject field isn't empty. preg_match performs a regular expression match. It's a very powerful PHP function to validate form fields and other strings - see PHP manual for details. */
-if (!preg_match("/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/", $email)) {
-  echo "<h4>Invalid email address</h4>";
-  echo "<a href='javascript:history.back(1);'>Back</a>";
-} elseif ($subject == "") {
+if ($subject == "") {
   echo "<h4>No subject</h4>";
   echo "<a href='javascript:history.back(1);'>Back</a>";
 }
